34a872649627d281fbb9e8dac56bb50a230135ae
lrb <lrb@lrbdemac-mini.local>
2015-12-28 14:27:13 +0800

    Initial Commit

---
+++ MyApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyApp
+//
+//  Created by lrb on 15/12/28.
+//  Copyright © 2015年 WanRong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyApp
+//
+//  Created by lrb on 15/12/28.
+//  Copyright © 2015年 WanRong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
